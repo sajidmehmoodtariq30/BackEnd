5265f0425aec18b829d4d3202fab9fac7ff8bb9d
Sajid M Tariq <sajidmehmoodtariq5@gmail.com>
2025-01-04 11:32:53 +0500

    minor change in first app

--- 01/index.js
+++ 01/index.js
@@ -4,6 +4,12 @@ const express = require('express')
 const app = express()
 // const port = 3000 // we will be using the port from the environment variable
 
+const data = {
+    name: 'Sajid Mehmood',
+    age: 18,
+    email: 'sajidmehmoodtariq5@gmail.com'
+}
+
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
@@ -11,8 +17,13 @@ app.get('/', (req, res) => {
 app.get('/login', (req, res) => {
     res.send('<div><h1>Enter you credentials</h1><input type = "text" /></div>')
 })
+
 app.get('/home', (req, res) => {
     res.send('<h1>Hello Sajid</h1>')
+})
+
+app.get('/data', (req, res) => {
+    res.json(data)
 })
 
 app.listen(process.env.PORT, () => {
